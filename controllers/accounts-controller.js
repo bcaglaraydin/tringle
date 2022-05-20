fd66ac6e8ba8ca9690e4e8983a3c9e4b970cb127
bcaglaraydin <berdancaglaraydin@gmail.com>
2022-05-20 16:32:32 +0300

    error handling

--- controllers/accounts-controller.js
+++ controllers/accounts-controller.js
@@ -1,53 +1,68 @@
 global.accounts = []
 
 export const getAccounts = (req, res) => {
-    res.status(200).send(accounts);
+    try {
+        res.status(200).send(accounts);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+
 }
 
 export const createAccount = (req, res) => {
+    try {
+        const account = req.body;
 
-    const account = req.body;
+        let valid_curr = ["TRY", "USD", "EUR"]
+        if (!valid_curr.includes(account.currencyCode)) {
+            res.status(400).send("Error: Invalid Currency!");
+            return;
+        }
 
-    let valid_curr = ["TRY", "USD", "EUR"]
-    if (!valid_curr.includes(account.currencyCode)) {
-        res.status(400).send("Error: Invalid Currency!");
-        return;
+        let valid_type = ["individual", "corporate"]
+        if (!valid_type.includes(account.accountType)) {
+            res.status(400).send("Error: Invalid account type!");
+            return;
+        }
+
+        if (accounts.some(acc => acc.accountNumber === account.accountNumber)) {
+            res.status(400).send("Error: accountNumber should be unique!");
+            return;
+        }
+
+        if (isNaN(account.accountNumber)) {
+            res.status(400).send("Error: accountNumber should be an integer!");
+            return;
+        }
+
+        accounts.push({ ...account, balance: "0.00" });
+        res.status(200).send();
+    } catch (error) {
+        res.status(500).send(error);
     }
 
-    let valid_type = ["individual", "corporate"]
-    if (!valid_type.includes(account.accountType)) {
-        res.status(400).send("Error: Invalid account type!");
-        return;
-    }
 
-    if (accounts.some(acc => acc.accountNumber === account.accountNumber)) {
-        res.status(400).send("Error: accountNumber should be unique!");
-        return;
-    }
-
-    if (isNaN(account.accountNumber)) {
-        res.status(400).send("Error: accountNumber should be an integer!");
-        return;
-    }
-
-    accounts.push({ ...account, balance: "0.00" });
-    res.status(200).send();
 };
 
 export const getAccount = (req, res) => {
+    try {
+        const accountNumber = req.params.accountNumber;
+        const account = accounts.find(acc => acc.accountNumber === accountNumber)
 
-    const accountNumber = req.params.accountNumber;
-    const account = accounts.find(acc => acc.accountNumber === accountNumber)
+        if (account) {
+            console.log(account)
+            res.status(200).send(account)
+            return;
+        }
+        else {
+            res.status(400).send("Error: account does not exist!")
+            return;
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
-    if (account) {
-        console.log(account)
-        res.status(200).send(account)
-        return;
-    }
-    else {
-        res.status(400).send("Error: account does not exist!")
-        return;
-    }
+
 
 };
 
--- controllers/transactions-controller.js
+++ controllers/transactions-controller.js
@@ -4,125 +4,146 @@ const addTransaction = (_accountNumber, _amount, _transationType) => {
     transactions.push({
         accountNumber: _accountNumber,
         amount: _amount,
-        transactioNType: _transationType,
+        transactionType: _transationType,
         createdAt: datetime.getFullYear() + '-' + (datetime.getMonth() + 1) + '-' + datetime.getDate() + " " + datetime.getHours() + ":" + datetime.getMinutes()
     })
 }
 
 
 export const makePayment = (req, res) => {
-    const sender = accounts.find(acc => acc.accountNumber === req.body.senderAccount);
-    const receiver = accounts.find(acc => acc.accountNumber === req.body.receiverAccount);
-    const amount = req.body.amount;
+    try {
+        const sender = accounts.find(acc => acc.accountNumber === req.body.senderAccount);
+        const receiver = accounts.find(acc => acc.accountNumber === req.body.receiverAccount);
+        const amount = req.body.amount;
 
-    if (!sender) {
-        res.status(400).send("Error: Sender Account does not exist!")
-        return;
-    }
-    if (!receiver) {
-        res.status(400).send("Error: Receiver Account does not exist!")
-        return;
-    }
-    if (isNaN(amount)) {
-        res.status(400).send("Error: amount should be a decimal!");
-        return;
-    }
-    if (sender.accountType != "individual" || receiver.accountType != "corporate") {
-        res.status(400).send("Error: Payments can only be wired from an individual account to a corporate account!")
-        return;
-    }
-    if (parseFloat(sender.balance) < parseFloat(amount)) {
-        res.status(400).send("Error: Insufficent balance!")
-        return;
+        if (!sender) {
+            res.status(400).send("Error: Sender Account does not exist!")
+            return;
+        }
+        if (!receiver) {
+            res.status(400).send("Error: Receiver Account does not exist!")
+            return;
+        }
+        if (isNaN(amount)) {
+            res.status(400).send("Error: amount should be a decimal!");
+            return;
+        }
+        if (sender.accountType != "individual" || receiver.accountType != "corporate") {
+            res.status(400).send("Error: Payments can only be wired from an individual account to a corporate account!")
+            return;
+        }
+        if (parseFloat(sender.balance) < parseFloat(amount)) {
+            res.status(400).send("Error: Insufficent balance!")
+            return;
+        }
+
+        accounts = accounts.map(acc =>
+            acc.accountNumber === receiver.accountNumber
+                ? { ...acc, balance: (parseFloat(acc.balance) + parseFloat(amount)).toFixed(2) }
+                : acc
+        );
+
+        accounts = accounts.map(acc =>
+            acc.accountNumber === sender.accountNumber
+                ? { ...acc, balance: (parseFloat(acc.balance) - parseFloat(amount)).toFixed(2) }
+                : acc
+        );
+
+        addTransaction(sender.accountNumber, amount, "payment")
+        addTransaction(receiver.accountNumber, amount, "payment")
+
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
     }
 
-    accounts = accounts.map(acc =>
-        acc.accountNumber === receiver.accountNumber
-            ? { ...acc, balance: (parseFloat(acc.balance) + parseFloat(amount)).toFixed(2) }
-            : acc
-    );
-
-    accounts = accounts.map(acc =>
-        acc.accountNumber === sender.accountNumber
-            ? { ...acc, balance: (parseFloat(acc.balance) - parseFloat(amount)).toFixed(2) }
-            : acc
-    );
-
-    addTransaction(sender.accountNumber, amount, "payment")
-    addTransaction(receiver.accountNumber, amount, "payment")
-
-    res.status(200).send()
 
 };
 
 export const makeDeposit = (req, res) => {
 
-    const amount = req.body.amount;
-    const account = accounts.find(acc => acc.accountNumber === req.body.accountNumber)
+    try {
+        const amount = req.body.amount;
+        const account = accounts.find(acc => acc.accountNumber === req.body.accountNumber)
 
-    if (!account) {
-        res.status(400).send("Error: Account does not exist!")
-        return;
+        if (!account) {
+            res.status(400).send("Error: Account does not exist!")
+            return;
+        }
+        if (account.accountType != "individual") {
+            res.status(400).send("Error: accountType should be individual!")
+            return;
+        }
+
+        if (isNaN(amount)) {
+            res.status(400).send("Error: amount should be a decimal!");
+            return;
+        }
+
+        accounts = accounts.map(acc =>
+            acc.accountNumber === account.accountNumber
+                ? { ...acc, balance: (parseFloat(acc.balance) + parseFloat(amount)).toFixed(2) }
+                : acc
+        );
+
+        addTransaction(account.accountNumber, amount, "deposit")
+
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
     }
-    if (account.accountType != "individual") {
-        res.status(400).send("Error: accountType should be individual!")
-        return;
-    }
 
-    if (isNaN(amount)) {
-        res.status(400).send("Error: amount should be a decimal!");
-        return;
-    }
 
-    accounts = accounts.map(acc =>
-        acc.accountNumber === account.accountNumber
-            ? { ...acc, balance: (parseFloat(acc.balance) + parseFloat(amount)).toFixed(2) }
-            : acc
-    );
-
-    addTransaction(account.accountNumber, amount, "deposit")
-
-    res.status(200).send()
 
 };
 
 
 export const makeWithdraw = (req, res) => {
+    try {
+        const amount = req.body.amount;
+        const account = accounts.find(acc => acc.accountNumber === req.body.accountNumber)
 
-    const amount = req.body.amount;
-    const account = accounts.find(acc => acc.accountNumber === req.body.accountNumber)
+        if (!account) {
+            res.status(400).send("Error: Account does not exist!")
+            return;
+        }
+        if (isNaN(amount)) {
+            res.status(400).send("Error: amount should be a decimal!");
+            return;
+        }
+        if (account.accountType != "individual") {
+            res.status(400).send("Error: accountType should be individual!")
+            return;
+        }
+        if (parseFloat(account.balance) < parseFloat(amount)) {
+            res.status(400).send("Error: Insufficent balance!")
+            return;
+        }
 
-    if (!account) {
-        res.status(400).send("Error: Account does not exist!")
-        return;
+        accounts = accounts.map(acc =>
+            acc.accountNumber === account.accountNumber
+                ? { ...acc, balance: (parseFloat(acc.balance) - parseFloat(amount)).toFixed(2) }
+                : acc
+        );
+
+        addTransaction(account.accountNumber, amount, "withdraw")
+
+        res.status(200).send()
+    } catch (error) {
+        res.status(500).send(error)
     }
-    if (isNaN(amount)) {
-        res.status(400).send("Error: amount should be a decimal!");
-        return;
-    }
-    if (account.accountType != "individual") {
-        res.status(400).send("Error: accountType should be individual!")
-        return;
-    }
-    if (parseFloat(account.balance) < parseFloat(amount)) {
-        res.status(400).send("Error: Insufficent balance!")
-        return;
-    }
 
-    accounts = accounts.map(acc =>
-        acc.accountNumber === account.accountNumber
-            ? { ...acc, balance: (parseFloat(acc.balance) - parseFloat(amount)).toFixed(2) }
-            : acc
-    );
 
-    addTransaction(account.accountNumber, amount, "withdraw")
-
-    res.status(200).send()
 
 };
 
 export const getTransactions = (req, res) => {
-    const accountNumber = req.params.accountNumber;
-    const transaction = transactions.filter(t => t.accountNumber === accountNumber)
-    res.status(200).send(transaction)
+    try {
+        const accountNumber = req.params.accountNumber;
+        const transaction = transactions.filter(t => t.accountNumber === accountNumber)
+        res.status(200).send(transaction)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+
 };
